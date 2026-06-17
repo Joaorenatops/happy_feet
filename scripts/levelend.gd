17f4c0c056a223a906586594e8dbde78623ce0a8
aluno_renato <pcw223951@gmail.com>
2026-06-17 17:10:07 -0300

    Agachado

--- scripts/levelend.gd
+++ scripts/levelend.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@export var next_level = ""
 
-func _on_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://scene/forest.tscn")
+
+func _on_body_entered(_body: Node2D) -> void:
+	get_tree().change_scene_to_file("res://scene/"+next_level+".tscn")
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,145 @@
 extends CharacterBody2D
-@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
+enum PlayerState {
+	idle,
+	walk,
+	jump,
+	duck
+}
+
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 80.0
 const JUMP_VELOCITY = -300.0
+var direction = 0
+var status: PlayerState
 
+func _ready() -> void:
+	go_to_idle_state()
 
 func _physics_process(delta: float) -> void:
+	
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+	match status:
+		PlayerState.idle:
+			idle_state()
+		PlayerState.walk:
+			walk_state()
+		PlayerState.jump:
+			jump_state()
+		PlayerState.duck:
+			duck_state()
+			
+	move_and_slide()
+			
+func go_to_idle_state():
+	status = PlayerState.idle
+	anim.play("idle")
+	
+func go_to_walk_state():
+	status = PlayerState.walk
+	anim.play("walk")
+	
+func go_to_jump_state():
+	status = PlayerState.jump
+	anim.play("jump")
+	velocity.y = JUMP_VELOCITY
+	
+func go_to_duck_state():
+	status = PlayerState.duck
+	anim.play("duck")
+	CollisionShape2D.shape.radius = 5
+	CollisionShape2D.shape.height = 5
+	CollisionShape2D.shape.position.y = 3
+	
+func exit_from_duck_state():
+	CollisionShape2D.shape.radius = 5
+	CollisionShape2D.shape.height = 10
+	CollisionShape2D.shape.position.y = 3
+	
+func idle_state():
+	move()
+	if velocity.x != 0:
+		go_to_walk_state()
+		return
+		
+	if Input.is_action_just_pressed("jump"):
+		go_to_jump_state()
+		return
+	
+	if Input.is_action_pressed("duck"):
+		go_to_duck_state()
+		return
+		
+func walk_state():
+	move()
+	if velocity.x == 0:
+		go_to_idle_state()
+		return
+		
+	if Input.is_action_just_pressed("jump"):
+		go_to_jump_state()
+		return
+	
+func jump_state():
+	move()
+	if is_on_floor():
+		if velocity.x == 0:
+			go_to_idle_state()
+		else:
+			go_to_walk_state()
+		return
+func duck_state():
+	update_direction() # Alterado: Adicionado para atualizar o lado do sprite
+	
+	if Input.is_action_just_released("duck"):
+		exit_from_duck_state()
+		go_to_idle_state()
+		return
+	
+func move():
+	update_direction()
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+func update_direction():
+	direction = Input.get_axis("left", "right")
+	
+	if direction < 0:
+		anim.flip_h = true
+	elif direction > 0:
+		anim.flip_h = false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func temp(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -22,15 +155,4 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if is_on_floor():
-		if direction > 0:
-			anim.flip_h = false
-			anim.play("walk")
-		elif direction < 0:
-			anim.flip_h = true
-			anim.play("walk")
-		else:
-			anim.play("idle")
-	else:
-		anim.play("jump")
-	move_and_slide()
+	
